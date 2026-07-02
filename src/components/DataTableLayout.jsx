c8fe6844314ba1e4134857b7ba567013c3b06b88
olaalam <cds.olaallam82330@alexu.edu.eg>
2026-07-02 12:27:32 +0200

    redesign pagentation

--- src/components/DataTableLayout.jsx
+++ src/components/DataTableLayout.jsx
@@ -123,11 +123,29 @@ export default function DataTable({
     return currentData;
   }, [data, searchValue, activeFilters, searchKeys, isBackendPagination]);
   const getPaginationItems = (current, total) => {
+    const delta = 2; // عدد الصفحات التي تظهر حول الصفحة الحالية
     const range = [];
+    const rangeWithDots = [];
+    let l;
+
     for (let i = 1; i <= total; i++) {
-      range.push(i);
+      if (i === 1 || i === total || (i >= current - delta && i <= current + delta)) {
+        range.push(i);
+      }
     }
-    return range;
+
+    for (let i of range) {
+      if (l) {
+        if (i - l === 2) {
+          rangeWithDots.push(l + 1);
+        } else if (i - l !== 1) {
+          rangeWithDots.push("...");
+        }
+      }
+      rangeWithDots.push(i);
+      l = i;
+    }
+    return rangeWithDots;
   };
   // --- منطق الترقيم ---
   const totalPagesLocal = Math.ceil(filteredData.length / itemsPerPage);
